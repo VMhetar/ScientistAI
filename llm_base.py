ffac6575235ec5ded319e76f55575f801539e3b2
VMhetar <mhetarvarad@gmail.com>
2026-01-09 09:01:42 +0530

    Created llm_base.py and updated code in it.

---
+++ llm_base.py
@@ -0,0 +1,34 @@
+import os
+import httpx
+import asyncio
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP('127.0.0.1')
+
+api_key = os.getenv('OPENROUTER_API_KEY')
+
+headers = {
+    'Auhorization': f'Bearer {api_key}',
+    'Content-type': 'application/json'
+}
+
+url = 'https://openrouter.ai/api/v1/chat/completions'
+prompt_base = f"""
+You are a research assistant. Help the researcher with multiple tasks and each time you are called for a specific task, work over it
+"""
+
+async def llm_call(prompt:str):
+    prompt =prompt_base
+    data = {
+        'messages':[
+            {
+                'role':'user',
+                'content':prompt
+            }
+        ]
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url=url, headers=headers, json=data)
+        result = response.json()
+        return result
