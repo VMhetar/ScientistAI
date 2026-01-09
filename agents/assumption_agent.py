34913c490b70484946b4c02387bfd0dcc3194bb1
VMhetar <mhetarvarad@gmail.com>
2026-01-09 20:34:27 +0530

    Update assumption_agent.py file

--- agents/assumption_agent.py
+++ agents/assumption_agent.py
@@ -1,7 +1,7 @@
 import os
 import httpx
 import asyncio
-import llm_base
+from llm_base import llm_call
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP('Assumption-Agent')
@@ -18,3 +18,8 @@ url = 'https://openrouter.ai/api/v1/chat/completions'
 prompt_base = f"""
     You are a research assistant whose work is to understand the papers and make assumtions out of them.
 """
+
+async def assumption_agent(prompt:str):
+    prompt = prompt_base
+    response = llm_call(prompt)
+    return response
